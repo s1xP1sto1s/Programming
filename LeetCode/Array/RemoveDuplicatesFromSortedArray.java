22a18426dcad1d315e2746789d2a5152c125dfa5
s1xP1sto1s <1720461057@qq.com>
2016-10-15 14:19:01 +0800

    Update RemoveDuplicatesFromSortedArray.java

--- LeetCode/Array/RemoveDuplicatesFromSortedArray.java
+++ LeetCode/Array/RemoveDuplicatesFromSortedArray.java
@@ -50,3 +50,19 @@ public class Solution {
         }
     }
 }
+/******************************Runtime 16ms***********************************/
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0 || nums.length==1)
+            return nums.length;
+        int index = 1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]>nums[index-1])
+            {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
