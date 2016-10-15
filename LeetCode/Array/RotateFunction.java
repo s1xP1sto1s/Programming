c16471f895d867391498325d5103a1f48eb4cdf1
s1xP1sto1s <1720461057@qq.com>
2016-10-15 13:44:54 +0800

    Update RotateFunction.java

--- LeetCode/Array/RotateFunction.java
+++ LeetCode/Array/RotateFunction.java
@@ -21,7 +21,7 @@ F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
 
 So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.
 */
-
+/*****************************Runtime 589ms**********************************/
 public class Solution {
     public int maxRotateFunction(int[] A) {
         int len = A.length;
