c8d194121cd3fec1fd407acad6ca6bddeff811e5
s1xP1sto1s <1720461057@qq.com>
2016-10-20 23:34:17 +0800

    Update and rename 1001.java to 1001(A+BFormat).java

--- PAT-A/1001(A+BFormat).java
+++ PAT-A/1001(A+BFormat).java
@@ -1,5 +1,5 @@
-1001. A+B Format (20)
 /*
+1001. A+B Format (20)
 Calculate a + b and output the sum in standard format -- that is, the digits must be separated into groups of three by commas 
 (unless there are less than four digits).
 
